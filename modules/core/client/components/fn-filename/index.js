0febf2760bbb19fc9fe990b98e8ccac3b7bcceeb
Max Wardle <max.wardle@gov.bc.ca>
2019-03-29 13:33:15 -0700

    EM-284: Added restrictions on document display names (#290)

--- modules/core/client/components/fn-filename/index.js
+++ modules/core/client/components/fn-filename/index.js
@@ -1,13 +1,14 @@
 'use strict';
 
 (function() {
-  var NOT_ALLOWED = 'These characters are not allowed: /?<>:*|":';
+  var NOT_ALLOWED = 'Some characters are not in the list of accepted cahracters for file names: letters, numbers, underscore, period, dash, and space.';
   var NO_CONTROL = 'Control characters are not allowed';
   var NO_PERIOD_OR_SPACE = "Names shouldn't begin with a period or spaces";
   var RESERVED = 'Can not use reserved file names like COM0, PRN, etc';
   var NO_ALL_PERIOD = 'Names can not be just periods';
   var NO_TRAILING_SPACE = 'Names can not end with spaces';
   var REQUIRED = 'Required';
+  var OVER_CAHAR_LIMIT = 'File nemes should be below 175 characters';
 
   angular.module('core')
   // fn-filename
@@ -57,7 +58,7 @@
         // Validation can happen twice. Once without the file extension and once with.
         // Make sure the tests work in both cases.
         // eslint-disable-next-line no-useless-escape
-        var illegalRe = /[\/\?<>\\:\*\|":]/g; // basic illegal filename characters
+        var illegalRe = /[^a-zA-Z0-9_\.\-\s]/g; // white list of accepted file name characters
         // eslint-disable-next-line no-control-regex
         var controlRe = /[\x00-\x1f\x80-\x9f]/g; // no control characters
         var windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
@@ -67,6 +68,14 @@
         var reservedEnd = /\s+$/; // no names with spaces at end without extension
         var reservedEndWExt = extension ? new RegExp('\\s+\\.' + extension + '$') : undefined; // no names with spaces before extension
         var result = '';
+
+        // remove file extension for character count
+        var fileName = input;
+        var fileNameArr = fileName.split(".");
+        if (fileNameArr[fileNameArr.length-1] == 'pdf'){
+          fileName = fileName.substring(0, fileName.length-4);
+        }
+
         if (input.match(illegalRe)) {
           result = NOT_ALLOWED;
         } else if (input.match(controlRe)) {
@@ -81,6 +90,8 @@
           result = RESERVED;
         } else if (input.match(reservedRe)) {
           result = NO_ALL_PERIOD;
+        }else if (fileName.length>=175) { //character count cannot be over 175
+          result = OVER_CAHAR_LIMIT;
         }
         return result;
       }
